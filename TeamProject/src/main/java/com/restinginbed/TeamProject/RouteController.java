59d78d0dcbdf313f28c5fc72ef80b68f8706a71e
Ashley Garcia <ashley.garcia.official@gmail.com>
2024-10-15 03:45:39 -0400

    build: RouteController (postgresql)

--- TeamProject/src/main/java/com/restinginbed/TeamProject/RouteController.java
+++ TeamProject/src/main/java/com/restinginbed/TeamProject/RouteController.java
@@ -1,84 +1,38 @@
 package com.restinginbed.TeamProject;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * This class contains all the API routes for the system.
  */
 @RestController
-@RequestMapping("/api")
 public class RouteController {
 
-  private HashMap<String, Item> items = new HashMap<>();
-  private HashMap<String, User> users = new HashMap<>();
-  private HashMap<String, Organization> organizations = new HashMap<>();
-
-  // Create an Item
-  @PostMapping("/createItem")
-  public ResponseEntity<String> createItem(@RequestBody Item item) {
-    items.put(item.getId(), item);
-    return new ResponseEntity<>("Item created successfully", HttpStatus.CREATED);
+  /**
+   * Redirects to the homepage.
+   * 
+   * @return A String containing the name of the html file to be loaded.
+   */
+  @GetMapping({"/", "index", "/home"})
+  public String index() {
+    return "Welcome, in order to make an API call direct your browser or Postman to an endpoint "
+        + "\n\n This can be done using the following format: \n\n http:127.0.0"
+        + ".1:8080/endpoint?arg=value";
   }
 
-  // Create a User
-  @PostMapping("/createUser")
-  public ResponseEntity<String> createUser(@RequestBody User user) {
-    users.put(user.getId(), user);
-    return new ResponseEntity<>("User created successfully", HttpStatus.CREATED);
-  }
+  @GetMapping(value = "/retrieveUser", produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<?> retrieveUser(@RequestParam(value = "userID") String deptCode) {
 
-  // Create an Organization
-  @PostMapping("/createOrganization")
-  public ResponseEntity<String> createOrganization(@RequestBody Organization organization) {
-    organizations.put(organization.getId(), organization);
-    return new ResponseEntity<>("Organization created successfully", HttpStatus.CREATED);
-  }
-
-  // Update an Item
-  @PatchMapping("/updateItem")
-  public ResponseEntity<String> updateItem(@RequestBody Item item) {
-    if (items.containsKey(item.getId())) {
-      items.put(item.getId(), item);
-      return new ResponseEntity<>("Item updated successfully", HttpStatus.OK);
-    } else {
-      return new ResponseEntity<>("Item not found", HttpStatus.NOT_FOUND);
-    }
-  }
-
-  // Update a User
-  @PatchMapping("/updateUser")
-  public ResponseEntity<String> updateUser(@RequestBody User user) {
-    if (users.containsKey(user.getId())) {
-      users.put(user.getId(), user);
-      return new ResponseEntity<>("User updated successfully", HttpStatus.OK);
-    } else {
-      return new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND);
-    }
-  }
-
-  // Update an Organization
-  @PatchMapping("/updateOrganization")
-  public ResponseEntity<String> updateOrganization(@RequestBody Organization organization) {
-    if (organizations.containsKey(organization.getId())) {
-      organizations.put(organization.getId(), organization);
-      return new ResponseEntity<>("Organization updated successfully", HttpStatus.OK);
-    } else {
-      return new ResponseEntity<>("Organization not found", HttpStatus.NOT_FOUND);
-    }
-  }
-
-  // Delete an Item
-  @DeleteMapping("/deleteItem")
-  public ResponseEntity<String> deleteItem(@RequestParam String itemId) {
-    if (items.containsKey(itemId)) {
-      items.remove(itemId);
-      return new ResponseEntity<>("Item deleted successfully", HttpStatus.OK);
-    } else {
-      return new ResponseEntity<>("Item not found", HttpStatus.NOT_FOUND);
-    }
   }
 
 }
