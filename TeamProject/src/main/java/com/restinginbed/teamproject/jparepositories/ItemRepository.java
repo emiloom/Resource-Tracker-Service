10b376f57fecb769853d092e419899002c12e0c4
djuarez2652 <djuarez0440@gmail.com>
2024-10-18 23:06:03 -0400

    style: fix style for ItemRepository class

--- TeamProject/src/main/java/com/restinginbed/teamproject/jparepositories/ItemRepository.java
+++ TeamProject/src/main/java/com/restinginbed/teamproject/jparepositories/ItemRepository.java
@@ -1,10 +1,14 @@
-package com.restinginbed.teamproject.jpa_repositories;
+package com.restinginbed.teamproject.jparepositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import com.restinginbed.teamproject.Item;
 import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Repository interface for performing CRUD operations on Item entities.
+ */
 public interface ItemRepository extends JpaRepository<Item, Integer> {
-	List<Item> findByNameContaining(String name);
+  List<Item> findByNameContaining(String name);
+
   List<Item> findByOrganizationId(Integer organizationId);
 }
