00e60140ea20de70b457e5869c498151e2a44d40
djuarez2652 <djuarez0440@gmail.com>
2024-10-18 22:58:01 -0400

    style: fix style for TeamProjectApplication and TeamProjectApplicationTests classes

--- TeamProject/src/main/java/com/restinginbed/teamproject/TeamProjectApplication.java
+++ TeamProject/src/main/java/com/restinginbed/teamproject/TeamProjectApplication.java
@@ -1,13 +1,17 @@
-package com.restinginbed.TeamProject;
+package com.restinginbed.teamproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The main entry point for the TeamProject application
+ * for Advanced SWE class.
+ */
 @SpringBootApplication
 public class TeamProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TeamProjectApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(TeamProjectApplication.class, args);
+  }
 
 }
