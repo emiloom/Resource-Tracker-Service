c5dc374efde83bf13b354d85dd973c25a2f0ba6e
djuarez2652 <djuarez0440@gmail.com>
2024-11-30 00:22:45 -0500

    style: fix style for googleauthservice file

--- TeamProject/src/main/java/com/restinginbed/teamproject/service/GoogleAuthService.java
+++ TeamProject/src/main/java/com/restinginbed/teamproject/service/GoogleAuthService.java
@@ -5,10 +5,23 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Scanner;
 
+/**
+ * Service class for handling Google OAuth2 authentication.
+ *
+ * <p>Provides methods to validate access tokens and retrieve user information.
+ */
 public class GoogleAuthService {
 
   private static final String TOKEN_INFO_URL = "https://www.googleapis.com/oauth2/v3/tokeninfo";
 
+  /**
+   * Retrieves the user ID from the provided Google OAuth2 access token.
+   *
+   * @param token the access token to validate and extract user information.
+   * @return the user ID as a string if the token is valid.
+   * @throws Exception if there is an error connecting to the token info
+   *                   endpoint or processing the response.
+   */
   public String getUserIdFromToken(String token) throws Exception {
     URL url = new URL(TOKEN_INFO_URL + "?access_token=" + token);
     HttpURLConnection connection = (HttpURLConnection) url.openConnection();
