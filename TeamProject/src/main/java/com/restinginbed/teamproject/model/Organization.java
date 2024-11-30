1f7d7f0adceb965ce05b96fa90049d27d11928b2
David Juarez <85270302+djuarez2652@users.noreply.github.com>
2024-11-30 00:47:52 -0500

    fix: fix pmd errors (#47)

--- TeamProject/src/main/java/com/restinginbed/teamproject/model/Organization.java
+++ TeamProject/src/main/java/com/restinginbed/teamproject/model/Organization.java
@@ -3,8 +3,6 @@ package com.restinginbed.teamproject.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import java.io.Serial;
 import java.io.Serializable;
